542008186e727b007182b3b2bcc607d0d51b1b86
GravesG <18870224341@163.com>
2019-01-02 21:38:17 +0800

    role

--- src/main/java/com/mmall/model/SysRole.java
+++ src/main/java/com/mmall/model/SysRole.java
@@ -1,7 +1,16 @@
 package com.mmall.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import java.util.Date;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class SysRole {
     private Integer id;
 
